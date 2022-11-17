9bd7afb436fa20f3354cb281c64dc22c04b67c7b
游诗成 <78134919+ysc-yngtxh@users.noreply.github.com>
2022-11-17 22:56:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '421a647a511529709be42afcf4ee16bb',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '湖北',
+  CITY: '武汉',
 
   USERS: [
     {
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '19p9PLVgYEUCOwqzaq4buJJnN4YJqkFbYAHmLQkHntc',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o_gR_5xD5kjYz1xfnAFABWBHSAKA',
     }
   ],
 
