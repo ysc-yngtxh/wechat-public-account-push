e8c735cb50ba5e2e5fd65852cd523ae866c113e6
游诗成 <78134919+ysc-yngtxh@users.noreply.github.com>
2022-11-17 23:26:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o_gR_52Wy7pJMBzItISrtbImmuaA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '19p9PLVgYEUCOwqzaq4buJJnN4YJqkFbYAHmLQkHntc',
+      useTemplateId: 'VJmUJxy0Ze4v27_7Pr0WokudqWPW4QgZFhGxcQLdZ78',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-11',
       festivals: [
